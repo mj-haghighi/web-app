ad580f7a3e655b9ee60867baa13cf6fbe38e9f88
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-10-29 15:27:15 +0330

    Implement 'Button' as component

---
+++ src/components/button/button.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react'
+import './style.css'
+
+const Button = (props) => {
+    const { children, onClick, className, style } = props
+    const [styles, setStyle] = useState('')
+
+    const onMouseEnter = () => {
+        setStyle('shadow')
+    }
+
+    const onMouseLeave = () => {
+        setStyle('')
+    }
+
+    const onMouseDown = () => {
+        setStyle('')
+    }
+
+    const onMouseUp = () => {
+        setStyle('shadow')
+    }
+
+    return (
+        <button
+            onMouseEnter={onMouseEnter}
+            onMouseLeave={onMouseLeave}
+            onMouseDown={onMouseDown}
+            onMouseUp={onMouseUp}
+            style={style}
+            className={`flex-1 ease button marg-1 padd-1 round-1 txt-col-pri bold ${styles} ${className}`} onClick={onClick}>
+            {children}
+        </button>
+    )
+}
+
+export default Button
