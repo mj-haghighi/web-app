03b2a8ce3913e0fe17df21f68aa97691e43402bc
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-10-29 14:03:14 +0330

    Preview 'LoginPage' in 'web-app'

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
+import { LoginPage } from 'pages'
 function App() {
   return (
-    <div style={{
-      display: 'flex',
-      flex: 1,
-      fontSize: '50px',
-      justifyContent: 'center',
-      fontWeight: 'bold',
-      marginTop: '50px'
-    }}>
-      ;) سلام
-    </div>
+    <LoginPage />
   );
 }
 
