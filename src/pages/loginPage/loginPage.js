1736c767948f67db36e7da804c66d150eb7cd6f2
mj-haghighi <mohammadjavad.developer@gmail.com>
2021-10-29 14:02:32 +0330

    Init 'LoginPage' to preview empty 'Paper'

---
+++ src/pages/loginPage/loginPage.js
@@ -0,0 +1,14 @@
+import { Paper } from 'components';
+
+const LoginPage = (props) => {
+    return (
+        <div className="flex jc-center">
+            <Paper
+                className="margv-3"
+                style={{ minWidth: '300px', minHeight: '500px' }}
+            />
+        </div>
+    );
+}
+
+export default LoginPage;
